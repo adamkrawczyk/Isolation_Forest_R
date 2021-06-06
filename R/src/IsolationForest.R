3873b7dbe0be60435c0169c548a5e2c8279ee00c
Khasreto <adam-krawczyk@outlook.com>
2021-06-06 22:23:52 +0200

    bugged algo

--- R/src/IsolationForest.R
+++ R/src/IsolationForest.R
@@ -1,68 +1,196 @@
 library(R6)
-# setClass("IsolationForest", slots = list(trees_num = "numeric", id = "numeric", test = "numeric"))
+library(data.tree)
 
-# setMethod("show",
-# "IsolationForest",
-# function(object) {
-#   cat("trees_num:", object@trees_num, "\n")
-#   cat("Id:", object@id, "\n")
-#   cat("Contact:", object@test, "\n")
-# }
-# )
-# setMethod("testMethodIFR",
-# "IsolationForest",
-# function(object) {
-#   cat("trees_num:", object@trees_num, "\n")
-#   cat("Id:", object@id, "\n")
-#   cat("Contact:", object@test, "\n")
-# }
-# )
+normalize <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
+computeC <- function(n) {
+  return(2 * (log(n - 1) + 0.5772156649) - (2 * (n - 1) / n))
+}
 
 
+IsolationForest = R6::R6Class(
+
+  public = list(
+
+    sample_size = NULL
+    , num_trees = NULL
+    , replace = NULL
+    , seed = NULL
+    , nproc = NULL
+    , respect_unordered_factors = NULL
+    , max_depth = NULL
+    , forest = NULL
+    , scores = NULL
+    , status = "not_initialized"
+    , trees = NULL
+    , model = NULL
+    ,
+    initialize = function(sample_size = 256
+                          , num_trees = 10
+                          , replace = FALSE
+                          , seed = 101
+                          , nproc = NULL
+                          , respect_unordered_factors = NULL
+                          , max_depth = ceiling(log2(sample_size))
+                          ) {
+      self$sample_size = sample_size
+      self$num_trees = num_trees
+      self$replace = replace
+      self$seed = seed
+      self$nproc = nproc
+      self$respect_unordered_factors = respect_unordered_factors
+      self$max_depth = max_depth
+      self$status = "not_trained"
+    },
 
 
+  fit = function(data, numberOfTrees = self$num_of_trees, subsetSize = self$sample_size) {
+    self$status = "not_trained"
+    maxTreeDepth = log(subsetSize, 2)
+    if (subsetSize > nrow(data)) {
+      subsetSize = nrow(data)
+      warning("Subset size bigger than number of data rows, 
+            changing subset size to number of data rows")
+    }
+    for (i in 1:numberOfTrees) {
+      tree <- Node$new("tree")
+      subSet <- data[sample(nrow(data), subsetSize),]
+      #print(paste0("Building tree: ", i))
+      self$buildTree(tree, subSet, maxTreeDepth, 0)
+      ##print("children in building forest")
+      ##print(tree$children)
+      trees = append(trees, tree)
+      # #print(trees)
+    }
+    model = structure(list(trees = trees), class = "isolationForestClass")
+    # return(trees)
+    # return(model)
+    self$trees = trees
+    self$model = model
+    self$status = "trained"
+  },
 
+  buildTree = function(node, data, remainingDepth, actualDepth) {
+    ##print(data[1:10,])
+    node$obsCount <- nrow(data)
+    ##print(paste0("remainingDepth: ", remainingDepth))
+    if (node$obsCount == 0) {
+      child <- node$AddChild('breakpoint')
+      child$feature <- ' '
+      child$obsCount <- 0
+      child$depth <- actualDepth
+      child$leaf <- 1
 
-IsolationForest <- R6::R6Class(
-  classname = "isolationForest",
+    }
+    else if (node$obsCount == 1 | remainingDepth == 0) {
+      #construct leaf when 1 object in node or max depth reached
+      child <- node$AddChild('breakpoint')
+      node$feature <- tail(names(data), 1)
+      child$obsCount <- 1
+      child$feature <- ''
+      child$depth <- actualDepth
+      child$leaf <- 1
+    }
 
-    public = list(
-      num_of_trees = NULL,
-      subsampling_size = NULL,
-      max_depth = NULL,
-      forest = NULL,
-      scores = NULL,
-      status = "uninitialized",
+    else {
 
-      initialize = function(num_of_trees = 100, subsampling_size = 256, num_seed = 100, max_depth = ceiling(log2(subsampling_size))) {
+      numberOfFeatures <- length(names(data))
+      # set.seed(123)
+      whichFeature <- sample(1:numberOfFeatures, 1, replace = TRUE)
+      feature <- names(data)[whichFeature]
+      vector <- data[, whichFeature, drop = FALSE]
+      if (var(vector, na.rm = TRUE) == 0) {
+        #create leaf if for a given feature there is only 1 value
+        child <- node$AddChild('same_values')
+        node$feature <- tail(names(data), 1)
+        child$obsCount <- nrow(data)
+        child$feature <- ''
+        child$depth <- actualDepth
+        child$leaf <- 1
+      }
+      else {
+        remainingDepth <- remainingDepth - 1
+        actualDepth <- actualDepth + 1
+        node$feature <- feature
+        minValue <- min(vector, na.rm = TRUE)
+        maxValue <- max(vector, na.rm = TRUE)
+        # set.seed(123)
+        splitPoint <- runif(1, min = minValue, max = maxValue)
+        node$splitPoint <- splitPoint
+        childObs <- split(data,
+                          data[, feature] > splitPoint,
+                          drop = TRUE)
+        for (i in 1:length(childObs)) {
+          #construct a child having the name of that feature value (e.g. 'red')
+          name <- paste(feature, toString(splitPoint), "greater", names(childObs)[i], sep = " ")
+          child <- node$AddChild(name)
+          child$leaf <- 0
+          self$buildTree(child, childObs[[i]], remainingDepth, actualDepth)
+        }
 
+      }
+    }
+  }
+  ,
 
+  pathLength = function(tree, row) {
+    # print(tree$children)
 
-        self$subsampling_size = subsampling_size
-        self$num_of_trees = num_of_trees
-        # self$num_seed = num_seed
-        self$max_depth = max_depth
-        self$status = "not_trained"
-      },
-
-      getScores = function(data) {
-
-        print("getting scores")
-      },
-
-      getiForest = function(data) {
-
-        print("creating iForest")
-      },
-
-      getiTrees = function(data) {
-
-        print("creating iTrees")
+    #############aaaaaaaaaah why this is null????????????????????????????
+    if (tree$children[[1]]$leaf == 1) {
+      if (tree$children[[1]]$obsCount == 1) {
+        return(tree$children[[1]]$depth)
+      } else {
+        return(tree$children[[1]]$depth + computeC(tree$children[[1]]$obsCount))
       }
 
+    }
+    #print("searching tree")
+    feature <- tree$feature
+    splitPoint <- tree$splitPoint
+    if (row[1, feature] > splitPoint) {
+      name <- paste(feature, toString(splitPoint), "greater", "TRUE", sep = " ")
+    }
+    else {
+      name <- paste(feature, toString(splitPoint), "greater", "FALSE", sep = " ")
+    }
+    child <- tree$children[[name]]
+    return(self$pathLength(child, row))
+  }
+  ,
+  predict = function(data, subsetSize = 256) {
 
+    if (subsetSize > nrow(data)) {
+      subsetSize = nrow(data)
+      warning("Subset size bigger than number of data rows, 
+              changing subset size to number of data rows")
+    }
 
-    )
+    predictions = list()
+    n = subsetSize
+    c_const = computeC(n)
+
+    for (i in 1:nrow(data)) {
+      averageDepth <- 0
+      for (j in 4:length(self$trees)) {
+        depth <- self$pathLength(self$trees[[j]], data[i,])
+        averageDepth <- averageDepth + depth
+      }
+      averageDepth <- averageDepth / (length(self$trees) - 3)
+
+      if (2 ^ (-averageDepth / c_const) > 0.5) {
+        tmpPrediction = 1
+      } else {
+        tmpPrediction = 0
+      }
+      # predictions <- append(predictions, 2^(-averageDepth/c_const))
+      predictions <- append(predictions, tmpPrediction)
+    }
+
+    return(unlist(predictions))
+  }
 
 )
-
+)
--- R/src/test.R
+++ R/src/test.R
@@ -1,10 +1,43 @@
 source("IsolationForest.R")
+# install.packages("data.tree")
+library(ggplot2)
+# library(data.tree)
 
 # get_trees <- getiTrees(2,12,2)
 
-ifr <- IsolationForest$new()
-# ifr
-ifr$getiForest()
-ifr$getiTrees()
-ifr$getScores()
+# ifr <- IsolationForest$new()
+# # ifr
+# ifr$getiForest()
+# ifr$getiTrees()
+# ifr$getScores()
 # just to check 
+
+
+n = 1000
+Var1 = c(rnorm(n, 0, 0.5), rnorm(n*0.1, -2, 1))
+Var2 = c(rnorm(n, 0, 0.5), rnorm(n*0.1,  2, 1))
+outliers = c(rep(0, n), rep(1, (0.1*n))) + 3
+data = data.frame(Var1, Var2)
+
+#plot data
+ggplot(data, aes(x = Var1, y = Var2)) + 
+  geom_point(shape = 1, alpha = 0.5) +
+  labs(x = "x", y = "y") +
+  labs(alpha = "", colour="Legend")
+
+#create isolation forest using isolationForest function from solitude package with default parameters
+
+iforest <- IsolationForest$new()
+# iforest <- isolationForest(data,50)
+
+#predict outliers within dataset
+data$fit <- iforest$fit(data, 1)
+data$pred <- iforest$predict(data)
+print(data$pred)
+data$outlier <- as.factor(ifelse(data$pred >=0.55, "outlier", "normal"))
+
+#plot data again with outliers identified
+ggplot(data, aes(x = Var1, y = Var2, color = outlier)) + 
+  geom_point(shape = 1, alpha = 0.5) +
+  labs(x = "x", y = "y") +
+  labs(alpha = "", colour="Legend")
